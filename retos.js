13606423ba9549943f2c2ecdd6dd8ab3596e3163
TheSerch07 <jejog50@gmail.com>
2023-01-02 21:13:31 -0500

    first commit

---
+++ retos.js
@@ -0,0 +1,8 @@
+function operacionMat(num1, num2) {
+    let result1 = num1 - num2
+    let result2 = num1 + num2
+    return result1.toString() + result2.toString()
+}
+
+console.log(operacionMat(5, 4))
+console.log(operacionMat(8, 2))
